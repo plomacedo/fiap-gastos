356e9b8634b9422ed03c413c7cb3eb9d20aa0d5f
Rafael Ross <rafaellross@gmail.com>
2023-04-29 11:26:27 -0300

    Adicionada conversão de moedas ao controller de expenses

--- src/main/java/com/fiap/webservices/Gastos/GastosApplication.java
+++ src/main/java/com/fiap/webservices/Gastos/GastosApplication.java
@@ -1,15 +1,26 @@
 package com.fiap.webservices.Gastos;
 
+import com.fiap.webservices.Gastos.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @SpringBootApplication
-public class GastosApplication {
+public class GastosApplication implements CommandLineRunner {
 
+	@Autowired
+	private UserRepository userRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(GastosApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		this.userRepository.findAll().forEach(user -> {
+			System.out.println(user.toString());
+		});
+	}
 }
